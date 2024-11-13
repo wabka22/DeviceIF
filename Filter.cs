47a8600a78313638b06df1977c5f048ed9fc9c21
Alexey2013 <alexey.chistov10@gmail.com>
2024-11-13 13:27:59 +0300

    filter

--- Filter.cs
+++ Filter.cs
@@ -4,32 +4,36 @@ using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 
 //https://dsp.stackexchange.com/questions/11290/linear-phase-notch-filter-band-reject-filter-implementation-in-c
+//режекторный фильтр второго порядка
 
 public class NotchFilter
 {
     private double[] a = new double[3];
     private double[] b = new double[3];
+
     private double[] inputPrev = new double[2];
     private double[] outputPrev = new double[2];
 
     public NotchFilter(double notchFreq)
     {
-        double omega = 2 * Math.PI * notchFreq / 500.0;
-        double alpha = Math.Sin(omega) / (2 * 50.0);
+
+        double angularFeq = 2 * Math.PI * notchFreq / 500.0;
+        double freqWidth = Math.Sin(angularFeq) / (2 * notchFreq);
 
         b[0] = 1;
-        b[1] = -2 * Math.Cos(omega);
+        b[1] = -2 * Math.Cos(angularFeq);
         b[2] = 1;
 
-        a[0] = 1 + alpha;
-        a[1] = -2 * Math.Cos(omega);
-        a[2] = 1 - alpha;
+        a[0] = 1 + freqWidth;
+        a[1] = -2 * Math.Cos(angularFeq);
+        a[2] = 1 - freqWidth;
 
         for (int i = 0; i < 3; i++)
         {
             b[i] /= a[0];
             a[i] /= a[0];
         }
+
     }
 
     public double Apply(double input)
