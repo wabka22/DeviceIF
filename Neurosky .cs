e7fcdf078e1c97b83c083dbf5592dc648af12693
Audice <karchkov.denis@mail.ru>
2024-12-09 09:23:58 +0300

    aaaaa

--- Neurosky .cs
+++ Neurosky .cs
@@ -1,31 +1,37 @@
 ﻿using System;
-using NeuroSky.ThinkGear;
+using Neurosky;
+using ThinkGearNET;
+//https://www.codeproject.com/Articles/567963/ThinkUino-Project
 
 class Program
 {
+
+    private bool comando = false;
+
+    private static string ARDUPORT = "COM5"; //declare the arduino serial port
+    private static string MINDPORT = "COM5"; //declare the Mindwave serial port
+
     static void Main(string[] args)
     {
-        ThinkGearClient client = new ThinkGearClient();
+        string error = "";
+        ThinkGearWrapper thinkGearWrapper = new ThinkGearWrapper();
+        if (thinkGearWrapper.Connect(MINDPORT, 57600, true)) //check if the Mindwave is connected
+        {
+            thinkGearWrapper.EnableBlinkDetection(true); //enable the eye blink on the eSense protocol
+            thinkGearWrapper.ThinkGearChanged += new EventHandler<ThinkGearChangedEventArgs>(
+              thinkGearWrapper_ThinkGearChanged); //capture the event when a new data is incoming
+        }
+        else
+            error = "Could not connect to headset";
 
-        try
-        {
-            // Подключение к устройству
-            client.Connect("COM5");
-            client.OnDataReceived += (sender, e) =>
-            {
-                Console.WriteLine($"Attention: {e.Attention}, Meditation: {e.Meditation}");
-            };
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-        finally
-        {
-            client.Disconnect();
-        }
+
+
+
+    }
+
+    private static void thinkGearWrapper_ThinkGearChanged(object sender, ThinkGearChangedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
