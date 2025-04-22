be1c58dc56048cacd16cf6e657fc577bb8db2be8
Alexey Chistov <alexey.chistov10@gmail.com>
2025-04-22 19:57:59 +0300

    folder

---
+++ Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Filtering.DataSources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeviceIF
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
